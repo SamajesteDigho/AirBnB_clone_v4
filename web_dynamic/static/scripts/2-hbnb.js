3f54e41288d9eda3192923767c7b8cbef1a85dea
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-06-16 14:55:48 +0800

    Exo 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,9 +1,17 @@
 #!/usr/bin/node
 // The 1-hbnb script
-const checkedAmenities = {};
+let checkedAmenities = {};
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+  if (data.status == 'OK') {
+    $('DIV#api_status').addClass('available');
+  } else {
+    $('DIV#api_status').removeClass('available');
+  }
+});
+
 window.onload = function () {
   $('DIV.amenities DIV.popover UL LI INPUT').on('click', function () {
-    const dataId = this.getAttribute('data-id');
+    let dataId = this.getAttribute('data-id');
     const dataName = this.getAttribute('data-name');
     if (dataId in checkedAmenities) {
       delete checkedAmenities[dataId];
